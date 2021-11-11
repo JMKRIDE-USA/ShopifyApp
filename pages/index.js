3b03923f4b959265fee071e8b9137911949787b0
Jeffdude <jeffmilling@gmail.com>
2021-11-11 15:34:18 -0800

    Got the subscription working finally!

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,23 @@
 import { Heading, Page, Button } from "@shopify/polaris";
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@apollo/client';
+import { node } from "prop-types";
+import React from "react";
 
-export const ACCESS_SCOPE_QUERY = gql`
+const GET_ACCESS_SCOPES = gql`
   query AccessScopeQuery {
     app {
       installation {
         accessScopes {
-          description,
           handle
         }
       }
     }
   }
 `
-export const ORDER_QUERY = gql`
-  query OrdersQuery {
-    orders(first: 10){
-      edges {
-        node {
-          id,
-          createdAt,
-          discountCode,
-        }
-      }
-    }
-  }
-`;
-// ... on WebhookHttpEndpoint {
-//   callbackUrl
-// }
-// ... on WebhookPubSubEndpoint {
-//   pubSubTopic
-// }
-
 const GET_SUBSCRIPTIONS = gql`
-  query webhookSubscriptions {
-    webhookSubscriptions(first: 10){
+  query webhookSubscriptions($first: Int!) {
+    webhookSubscriptions(first: $first){
       edges {
         node {
           id,
@@ -50,27 +31,60 @@ const GET_SUBSCRIPTIONS = gql`
   }
 `
 const CREATE_SUBSCRIPTION = gql`
-  mutation {
+  mutation eventBridgeWebhookSubscriptionCreate(
+    $webhookSubscription: EventBridgeWebhookSubscriptionInput!
+  ){
     eventBridgeWebhookSubscriptionCreate(
       topic: ORDERS_CREATE
-      webhookSubscription: {
-        arn: "arn:aws:events:us-east-2::event-source/aws.partner/shopify.com/6034237/AmbassadorSiteSource"
-        format: JSON
-      }
+      webhookSubscription: $webhookSubscription
     ) {
       webhookSubscription {
         id
+        topic
+        format
       }
       userErrors {
+        field
+        message
+      }
+    }
+  }
+`
+const DELETE_SUBSCRIPTION = gql`
+  mutation webhookSubscriptionDelete($id: ID!) {
+    webhookSubscriptionDelete(id: $id) {
+      userErrors {
+        field
         message
       }
     }
   }
 `
 
+const SubscriptionItem = (deleteSubscription) => ({ node }) => (
+  <li key={node.id}>
+    <p>{node.id} : {node.topic}</p>
+    <Button destructive onClick={() => deleteSubscription({variables: {id: node.id}})}>Delete</Button>
+  </li>
+)
+
 function Index() {
-  const { loading: queryLoading, error: queryError, data: queryData } = useQuery(GET_SUBSCRIPTIONS);
-  const [mutateFunction, { data: mutationData, loading: mutationLoading, error: mutationError }] = useMutation(CREATE_SUBSCRIPTION);
+  const accessScopeQuery = useQuery(GET_ACCESS_SCOPES);
+  const { loading: queryLoading, error: queryError, data: queryData } = useQuery(GET_SUBSCRIPTIONS, {variables: {first: 10}});
+  const [mutateFunction, { data: mutationData, loading: mutationLoading, error: mutationError }] = useMutation(
+    CREATE_SUBSCRIPTION, {
+      variables: {
+        webhookSubscription: {
+          arn: "arn:aws:events:us-east-2::event-source/aws.partner/shopify.com/6034237/AmbassadorSiteSource",
+          format: "JSON"
+        }
+      },
+      refetchQueries: ["webhookSubscriptions"],
+    }
+  );
+
+  const [deleteSubscription, mutationQuery] = useMutation(DELETE_SUBSCRIPTION, {refetchQueries: ["webhookSubscriptions"]})
+  console.log(mutationQuery)
 
   console.log({ queryLoading, queryError, queryData });
   console.log({ mutationData, mutationLoading, mutationError });
@@ -78,7 +92,18 @@ function Index() {
   return (
     <Page>
       <Heading>JMKRIDE Custom Shopify App</Heading>
-      <p>{JSON.stringify(queryData)}</p>
+      {accessScopeQuery.data && 
+        <React.Fragment>
+          <p>AccessScopes:</p>
+          <ul>{(accessScopeQuery.data.app.installation.accessScopes || []).map(scope => <li key={scope.handle}>{scope.handle}</li>)}</ul>
+        </React.Fragment>
+      }
+      {queryData && 
+        <React.Fragment>
+          Subscriptions:
+          <ul>{ queryData.webhookSubscriptions.edges.map(SubscriptionItem(deleteSubscription)) }</ul>
+        </React.Fragment>
+      }
       <Button onClick={mutateFunction}>Create Subscription</Button>
     </Page>
   )
